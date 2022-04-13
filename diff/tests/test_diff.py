690cabed7baa47efdd9b369dd95e904165531427
Fedor Pereverzev <maphtown@gmail.com>
2022-04-13 22:41:55 +0400

    step 4 done

--- diff/tests/test_diff.py
+++ diff/tests/test_diff.py
@@ -1,12 +1,10 @@
 from diff import generate_diff
 
 
-text = '''{\n  - follow: false\n    host: hexlet.io\n  - proxy: 123.234.53.22\n
-  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}'''
-
+text = open("./diff/tests/fixtures/result.txt", "r")
 fix1 = './diff/tests/fixtures/file1.json'
 fix2 = './diff/tests/fixtures/file2.json'
 
 
 def test_diff():
-    assert generate_diff(fix1, fix2) == text
+    assert generate_diff(fix1, fix2) == text.read()
