d753df66b8123b717e34746d71b63fc8ebb73e4f
Fedor Pereverzev <maphtown@gmail.com>
2022-04-13 22:21:35 +0400

    added test action

--- diff/tests/test_diff.py
+++ diff/tests/test_diff.py
@@ -1,9 +1,12 @@
 from diff import generate_diff
 
 
-text = '{\n  - follow: false\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}'
+text = '''{\n  - follow: false\n    host: hexlet.io\n  - proxy: 123.234.53.22\n
+  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}'''
+
+fix1 = './diff/tests/fixtures/file1.json'
+fix2 = './diff/tests/fixtures/file2.json'
+
 
 def test_diff():
-    assert generate_diff('./diff/tests/fixtures/file1.json', './diff/tests/fixtures/file2.json') == text
-
-    
+    assert generate_diff(fix1, fix2) == text
